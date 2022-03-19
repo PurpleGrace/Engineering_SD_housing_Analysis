ec17b2cc9dbe383e384c76d0588531f04d2a76df
PurpleGrace <PurpleGrace@users.noreply.github.com>
2022-03-18 22:33:47 -0700

    update

--- Load_data_to_Database.py
+++ Load_data_to_Database.py
@@ -11,35 +11,163 @@ import mysql
 # import pymysql
 import geopy
 import os
+import csv
+import numpy as np
+
+#os.chdir('/Users/kristy/Documents/Data Science Material/Metis/7 Engineering/Engineering Project/Engineering_SD_housing_Analysis')
+
+# Load zillow data from zillow website
+def zillow_value_forcast():
+    if os.path.exists('data/zillow_value_forcast.csv'):
+        df_zillow_value_forcast = pd.read_csv('data/zillow_value_forcast.csv')
+    else:
+        try:
+            url  = "https://www.zillow.com/research/data/"
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content,features="lxml")
+            href = soup.find('a', attrs={'id':'home-values-forecasts-download-link'}).attrs['href']
+            print('Getting data from the following link:\n',href)
+            df_zillow_value_forcast = pd.read_csv(href)
+        except:
+            return print('Load zillow data failed')
+    return df_zillow_value_forcast
 
 
-def load_data(href):
+df_zillow_value_forcast = zillow_value_forcast()
+df_zillow_value_forcast = df_zillow_value_forcast[df_zillow_value_forcast.CountyName == "San Diego County"]
+df_zillow_value_forcast = df_zillow_value_forcast[['SizeRank','RegionName',
+                        'City','CountyName','BaseDate','2022-03-31',
+                        '2022-05-31','2023-02-28']]
+df_zillow_value_forcast.rename(columns = {'RegionName':'ZipCode'},
+                                inplace = True)
+df_zillow_value_forcast = df_zillow_value_forcast.where(pd.notnull(df_zillow_value_forcast), None)
+df_zillow_value_forcast = df_zillow_value_forcast.astype(str)
+df_zillow_value_forcast.dtypes
+
+
+# Load zillow value index dataset and only take San Diego County data
+def zillow_value_index():
+    if os.path.exists('data/zillow_value_index.csv'):
+        df_zillow_value_index = pd.read_csv('data/zillow_value_index.csv')
+    else:
+        try:
+            url  = "https://www.zillow.com/research/data/"
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content,features="lxml")
+            href = soup.find('a', attrs={'id':'home-values-forecasts-download-link'}).attrs['href']
+            print('Getting data from the following link:\n',href)
+            df_zillow_value_index = pd.read_csv(href)
+        except:
+            return print('Load zillow data failed')
+    return df_zillow_value_index
+
+
+df_zillow_value_index = zillow_value_index()
+Total_rigion = df_zillow_value_index.shape[0]
+df_zillow_value_index = df_zillow_value_index[df_zillow_value_index.CountyName == "San Diego County"]
+df_zillow_value_index = df_zillow_value_index[['SizeRank','RegionName','City','CountyName','2022-02-28']]
+df_zillow_value_index.rename(columns = {'RegionName':'ZipCode'},inplace = True)
+df_zillow_value_index = df_zillow_value_index.where(pd.notnull(df_zillow_value_index), None)
+df_zillow_value_index = df_zillow_value_index.astype(str)
+df_zillow_value_index.dtypes
+df_zillow_value_index.shape
+df_zillow_value_index.head()
+
+
+# Load zillow rent index dataset and only take San Diego County data
+def zillow_rent_index():
+    if os.path.exists('data/zillow_rental_index.csv'):
+        df_zillow_rent_index = pd.read_csv('data/zillow_rental_index.csv')
+    else:
+        try:
+            url  = "https://www.zillow.com/research/data/"
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content,features="lxml")
+            href = soup.find('a', attrs={'id':'home-values-forecasts-download-link'}).attrs['href']
+            print('Getting data from the following link:\n',href)
+            df_zillow_rent_index = pd.read_csv(href)
+        except:
+            return print('Load zillow data failed')
+    return df_zillow_rent_index
+
+
+df_zillow_rent_index = zillow_rent_index()
+df_zillow_rent_index.shape[0]
+df_zillow_rent_index = df_zillow_rent_index[df_zillow_rent_index.MsaName == "San Diego, CA"]
+df_zillow_rent_index = df_zillow_rent_index[['SizeRank','RegionName','2022-02']]
+df_zillow_rent_index.rename(columns = {'RegionName':'ZipCode'},inplace = True)
+df_zillow_rent_index = df_zillow_rent_index.where(pd.notnull(df_zillow_rent_index), None)
+df_zillow_rent_index = df_zillow_rent_index.astype(str)
+df_zillow_rent_index.dtypes
+df_zillow_rent_index.shape
+df_zillow_rent_index.head()
+
+
+
+# load airbnb data from insideairbnb website
+def get_airbnb_data():
+    try:
+        url  = "http://insideairbnb.com/get-the-data.html"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content,features="lxml")
+        href = soup.find('a', attrs={'href':'http://data.insideairbnb.com/united-states/ca/san-diego/2021-12-20/visualisations/listings.csv'}).attrs['href']
+        print('Getting data from the following link:\n',href)
+    except:
+        return print('Load airbnb data failed')
     return pd.read_csv(href)
 
+df_airbnb = get_airbnb_data( )
+df_airbnb.head(1)
+df_airbnb.dtypes
 
-def get_zillow_link():
-    url = "https://www.zillow.com/research/data/"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, features="lxml")
-    href = (soup.find('a',
-            attrs={'id': 'home-values-forecasts-download-link'}).attrs['href'])
-    print('Getting data from the following link:\n', href)
-    return href
+df_airbnb = df_airbnb[['id','neighbourhood','latitude','longitude','room_type','price','minimum_nights','number_of_reviews','reviews_per_month']]
 
 
-def get_airbnb_link():
-    url = "http://insideairbnb.com/get-the-data.html"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, features="lxml")
-    href = soup.find('a', attrs={'href': 'http://data.insideairbnb.com/united-states/ca/san-diego/2021-12-20/visualisations/listings.csv'}).attrs['href']
-    print('Getting data from the following link:\n', href)
-    return href
+
+# Check if df_coor_zip.csv extist
+if not os.path.exists('data/df_coor_zip.csv'):
+    # Transfer latitude and longitude to zipcode with geopy.Nominatim
+    geolocator = geopy.Nominatim(user_agent='user_agent')
+    zipcodes = []
+    for i in range(len(df_airbnb)):
+        info = geolocator.reverse((df_airbnb.iloc[i].latitude, df_airbnb.iloc[i].longitude))
+        if 'postcode' in info.raw['address'].keys():
+            zipcodes.append(info.raw['address']['postcode'])
+        else:
+            zipcodes.append('Unknown')
+
+    df_airbnb['zipcode'] = [x.split('-')[0] for x in zipcodes]
+    df_coor_zip = df_airbnb.loc[:,['id','latitude','longitude','zipcode']]
+    df_coor_zip.to_csv('df_coor_zip.csv')
+# else:
+#     df_coor_zip = pd.read_csv('data/df_coor_zip.csv')
+
+df_coor_zip = pd.read_csv('data/df_coor_zip.csv')
+df_airbnb['zipcode'] = df_coor_zip['zipcode']
+df_airbnb = df_airbnb.where(pd.notnull(df_airbnb), None)
+df_airbnb = df_airbnb.astype("str")
+
+df_airbnb.dtypes
+
+
+# load rental sample data
+
+df_mls_rental_sample = pd.read_csv('data/mls_rental.csv',nrows =10)
+df_mls_rental_sample = df_mls_rental_sample.astype(str)
+df_mls_rental_sample.dtypes
+
+
+# load selling sampe data
+df_mls_selling_sample = pd.read_csv('data/mls_selling.csv',nrows = 10)
+df_mls_selling_sample = df_mls_selling_sample.astype(str)
+df_mls_selling_sample.dtypes
+
 
 
 # reference https://github.com/kevinchiv/Predicting-Kickstarter-Success/blob/master/00%20-%20PostgreSQL%20Through%20Jupyter.ipynb
-
 def create_table_schema(dataframe, table_name):
     col_names = dataframe.dtypes.index.values
+
     dtypes = dataframe.dtypes.values
 
     table_name = table_name.lower()
@@ -49,7 +177,10 @@ def create_table_schema(dataframe, table_name):
     for i, col_name in enumerate(col_names):
 
         dtype = dtypes[i]
-        # col_name = col_name.lower().replace(' ', '_')
+        if col_name[0].isdigit():
+            col_name = "_"+col_name.lower().replace(' ', '_').replace('-', '_')
+        else:
+            col_name = col_name.lower().replace(' ', '_').replace('-', '_')
         create_table += "\t"
 
         # assign column types and default values of null
@@ -59,8 +190,8 @@ def create_table_schema(dataframe, table_name):
         elif dtype == 'object':
             create_table = create_table + col_name+" VARCHAR(255) DEFAULT NULL"
 
-        elif dtype == 'float64':
-            create_table = create_table + col_name+" DECIMAL(10,2) DEFAULT NULL"
+        elif dtype == 'float32':
+            create_table = create_table + col_name+" FLOAT(36) DEFAULT NULL"
 
         if i != len(col_names) - 1:
             create_table += ", \n"
@@ -78,9 +209,11 @@ def set_database(db_name):
             cursor = db_con.cursor()
 #             cursor.execute("SHOW DATABASES")
 #             print([x for x in cursor])
+            #cursor.execute(f"DROP DATABASE IF EXISTS {db_name}")
             cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name}")
             print("Database %s is created" %db_name)
-        db_con.close()
+        db_con.commit()
+
     except Error as e:
         print("Error while connecting to MySQL", e)
 
@@ -93,6 +226,10 @@ def create_tables(tables):
             cursor.execute(f'DROP TABLE IF EXISTS {table};')
             #data.to_sql(tb_name, db_con, if_exists='fail');
             cursor.execute(TABLES[table])
+        db_con.commit()
+
+
+
 
 def insert_table_statement(df, table_name):
 
@@ -103,10 +240,12 @@ def insert_table_statement(df, table_name):
     insert_statement = "INSERT INTO %s (" %table_name
 
     for i, col_name in enumerate(col_names):
+        if col_name[0].isdigit():
+            col_name = "_"+col_name.lower().replace(' ', '_').replace('-', '_')
+        else:
+            col_name = col_name.lower().replace(' ', '_').replace('-', '_')
 
-        col_name = col_name.lower().replace(' ', '_')
-
-        insert_statement = insert_statement + col_name
+        insert_statement = insert_statement + '`'+ col_name +'`'
         if i != col_n-1:
             insert_statement += ","
 
@@ -115,55 +254,64 @@ def insert_table_statement(df, table_name):
     return insert_statement
 
 
-def insert_data_to_table(data, tb_name):
-    db_con = mysql.connector.connect(host='localhost', user='root', password='root1234', database = 'SDRE_data')
+# Save a datafrme into database
+def insert_dataframe_to_table(data,tb_name):
+    db_con = mysql.connector.connect(host='localhost', user='root',password='root1234',database = 'SDRE_data')
 
     if db_con.is_connected():
         cursor = db_con.cursor()
 
-        insert_statement = insert_table_statement(data, tb_name)
+        insert_statement = insert_table_statement(data,tb_name)
         for i in range(len(data)):
-            cursor.execute(insert_statement, tuple(data.iloc[i].to_list()))
+            cursor.execute(insert_statement,tuple(data.iloc[i].to_list()))
+        db_con.commit()
+
+
+
+# insert csv file into table
+def insert_csv_into_table(df_sample,file, tb_name):
+
+    db_con = mysql.connector.connect(host='localhost', user='root',password='root1234',database = 'SDRE_data')
+    num_correct = 0
+    if db_con.is_connected():
+        cursor = db_con.cursor()
+        insert_statement = insert_table_statement(df_sample,tb_name)
+        with open(file,'r') as csv_file:
+            reader = csv.reader(csv_file,delimiter = ',')
+            next(reader)
+            for index, row in enumerate(reader):
+                cursor.execute(insert_statement, tuple(row))
+                try:
+                    cursor.execute(insert_statement, tuple(row))
+                    num_correct += 1
+                except Exception as error:
+                    with open(f'data/error/{tb_name}_error.csv', mode = 'a') as error_file:
+                        error_file.write(str(index+1))
+
+
         db_con.commit()
         db_con.close()
 
+# insert_csv_into_table(df_mls_selling_sample, "data/mls_selling.csv",'mls_selling_tb' )
+# insert_csv_into_table(df_mls_selling_sample,'data/mls_rental.csv','mls_rental_tb' )
 
-if  __name__ == '__main__':
-    zillow_link = get_zillow_link()
-    df_zillow = load_data(zillow_link)
+TABLES = {}
+TABLES['zillow_rent_index_tb']= create_table_schema(df_zillow_rent_index,'zillow_rent_index_tb')
+TABLES['zillow_value_index_tb']= create_table_schema(df_zillow_value_index,'zillow_value_index_tb')
+TABLES['zillow_value_forcast_tb']= create_table_schema(df_zillow_value_forcast,'zillow_value_forcast_tb')
 
-    airbnb_link = get_airbnb_link()
-    df_airbnb = load_data(airbnb_link)
-    # df_airbnb.drop(columns=['name'],inplace = True)
-    df_airbnb = df_airbnb[['id','neighbourhood','latitude','longitude','room_type','price','minimum_nights','reviews_per_month']]
+TABLES['airbnb_tb'] = create_table_schema(df_airbnb,'airbnb_tb')
+TABLES['mls_rental_tb'] = create_table_schema(df_mls_rental_sample,'mls_rental_tb')
+TABLES['mls_selling_tb'] = create_table_schema(df_mls_selling_sample,'mls_selling_tb')
 
-    # Check if df_coor_zip.csv extist
-    if os.path.exists('df_coor_zip.csv') is False:
-        # Transfer latitude and longitude to zipcode with geopy.Nominatim
-        geolocator = geopy.Nominatim(user_agent='user_agent')
-        zipcodes = []
-        for i in range(len(df_airbnb)):
-            info = geolocator.reverse((df_airbnb.iloc[i].latitude, df_airbnb.iloc[i].longitude))
-            if 'postcode' in info.raw['address'].keys():
-                zipcodes.append(info.raw['address']['postcode'])
-            else:
-                zipcodes.append('Unknown')
+set_database("SDRE_data")
+create_tables(TABLES)
+insert_dataframe_to_table(df_zillow_rent_index,tb_name = 'zillow_rent_index_tb')
+insert_dataframe_to_table(df_zillow_value_index,tb_name = 'zillow_value_index_tb')
+insert_dataframe_to_table(df_zillow_value_forcast,tb_name = 'zillow_value_forcast_tb')
+insert_dataframe_to_table(df_airbnb,tb_name = 'airbnb_tb')
+insert_csv_into_table(df_mls_selling_sample, "data/mls_selling.csv",'mls_selling_tb' )
+insert_csv_into_table(df_mls_rental_sample,"data/mls_rental.csv",'mls_rental_tb' )
 
-        df_airbnb['zipcode'] = [x.split('-')[0] for x in zipcodes]
-        df_coor_zip = df_airbnb.loc[:,['id','latitude','longitude','zipcode']]
-        df_coor_zip.to_csv('df_coor_zip.csv')
-    else:
-        df_coor_zip = pd.read_csv('df_coor_zip.csv')
-        df_airbnb['zipcode'] = df_coor_zip['zipcode']
 
-        df_airbnb = df_airbnb.where(pd.notnull(df_airbnb), None)
-        df_airbnb = df_airbnb.astype(str)
-
-    TABLES = {}
-    TABLES['zillow_tb'] = create_table_schema(df_zillow, 'zillow_tb')
-    TABLES['airbnb_tb'] = create_table_schema(df_airbnb, 'airbnb_tb')
-
-    set_database("SDRE_data")
-    create_tables(TABLES)
-    insert_data_to_table(df_zillow, tb_name='zillow_tb')
-    insert_data_to_table(df_airbnb, tb_name='airbnb_tb')
+print('All data are loaded into database')
--- SDRE_streamlit.py
+++ SDRE_streamlit.py
@@ -1,49 +1,224 @@
 #!pip install mysql-connector
+#pip install pydeck
 import streamlit as st
-import sqlalchemy
+import plotly.express as px
+import plotly.graph_objects as go
 import mysql.connector
 import pandas as pd
+import numpy as np
+import pydeck as pdk
+from Load_data_to_Database import *
 
-st.write('''
-### San Diego County Housing Market inquriry Dashboard:
-''')
+
+os.chdir('/Users/kristy/Documents/Data Science Material/Metis/7 Engineering/Engineering Project/Engineering_SD_housing_Analysis')
+
+st.set_page_config(
+     page_title="SDRE Dashboard",
+     page_icon=":bar_chart:",
+     layout="wide",
+     initial_sidebar_state="expanded",
+ )
+
+
+# -------- sidebar -----------------------
+st.sidebar.header("Please Input Inquery Info:")
 
 db_con = mysql.connector.connect(host='localhost', user='root',password='root1234',database = 'SDRE_data')
 if db_con.is_connected():
     print('Database connected')
 cursor = db_con.cursor()
 
-st.sidebar.header("Please input inquriry info:")
-inquiry_zip = st.sidebar.text_input('Input Zipcode:', value = '92121')
-inquriry_room_number = st.sidebar.number_input('Inpurt Bedroom numbers', value = 2)
-inquiry_bath_number =  st. sidebar.number_input('Input Bath numbers', value  = 2)
+query = ('''
+        SELECT vf.zipcode as zipcode,
+                vf.city as city,
+                vf._2022_03_31 as vf_2022_03_31,
+                vf._2022_05_31 as vf_2022_05_31,
+                vf._2023_02_28 as vf_2023_02_28,
+                vi._2022_02_28 as vi__2022_02_28,
+                ri._2022_02 as ri__2022_02
+        FROM zillow_value_forcast_tb AS vf
+        LEFT JOIN (zillow_value_index_tb AS vi,zillow_rent_index_tb AS ri)
+        ON (vf.zipcode = vi.zipcode And vf.zipcode = ri.zipcode) ;
+''')
+
+df_zillow = pd.read_sql_query(query, db_con)
+
+query = ('''
+        SELECT zipcode,count(id) as listings,avg(price), avg(number_of_reviews ) as avg_reviws
+        FROM airbnb_tb
+        GROUP BY zipcode
+ ''')
+
+airbnb_zip_info = pd.read_sql_query(query,db_con)
+
+query = ('''
+        SELECT * FROM mls_rental_tb
+ ''')
+
+df_rental = pd.read_sql_query(query,db_con)
+
+query = ('''
+        SELECT * FROM mls_selling_tb
+ ''')
+
+df_selling = pd.read_sql_query(query,db_con)
+
+query = ('''
+        SELECT * FROM mls_rental_tb
+ ''')
+
+df_rental= pd.read_sql_query(query,db_con)
 
 
-query = (f"SELECT * from zillow_tb where RegionName = {inquiry_zip }")
 
+query = ('''
+     SELECT * FROM airbnb_tb;
+''')
+df_airbnb= pd.read_sql_query(query,db_con)
+df_airbnb.head()
+
+
+query = ('''
+        ALTER TABLE airbnb_tb MODIFY longitude Float(15,8);
+ ''')
 cursor.execute(query)
-results = list(cursor.fetchall())
 
-#st.dataframe(results)
-
-pd.DataFrame(results)
-
-
-st.write(f"""
-Zipcode {inquiry_zip} is located in city of {results[0][4]}  the expected house market increase : {results[0][6]}%
-""")
-
-
-st.subheader(f"""
- Airbnb properties in zipcode {inquiry_zip}
-""")
-query = (f"SELECT id,latitude,longitude,room_type,price from airbnb_tb where zipcode = {inquiry_zip}")
-
+query = ('''
+         ALTER TABLE airbnb_tb MODIFY latitude Float(15,8);
+  ''')
 cursor.execute(query)
-results = cursor.fetchall()
 
-airbnb_set = pd.DataFrame(results,columns=['id','latitude','longitude','room_type','price'])
-airbnb_set['latitude'] = airbnb_set['latitude'].astype(float)
-airbnb_set['longitude'] = airbnb_set['longitude'].astype(float)
+query = ('''
+         SELECT * FROM airbnb_tb;
+  ''')
+df_airbnb= pd.read_sql_query(query,db_con)
+df_airbnb.head()
+db_con.close()
 
-st.map(airbnb_set.loc[:,['latitude','longitude']])
+
+# cursor.execute(query)
+# df_zillow= pd.DataFrame(cursor.fetchall())
+# df_zillow.columns = ['zipcode','city',]
+
+inquiry_zip = st.sidebar.selectbox('Input ZipCode:',options = df_zillow.zipcode.unique(),index=1)
+inquriry_room_number = st.sidebar.number_input('Inpurt Bedroom numbers', value = 1)
+inquiry_bath_number =  st.sidebar.number_input('Input Bath numbers', value  = 2)
+
+# city = df_zillow[df_zillow.zipcode ==inquiry_zip ].city
+# vf_2022_03_31 = df_zillow[df_zillow.zipcode ==inquiry_zip ].vf_2022_03_31
+# vf_2022_05_31 = df_zillow[df_zillow.zipcode ==inquiry_zip ].vf_2022_05_31
+# vf_2023_02_28 = df_zillow[df_zillow.zipcode ==inquiry_zip ].vf_2023_02_28
+# vi__2022_02_28 = df_zillow[df_zillow.zipcode ==inquiry_zip ].vi__2022_02_28
+# ri__2022_02 = df_zillow[df_zillow.zipcode ==inquiry_zip ].ri__2022_02
+
+df_z_zip  = df_zillow.query('zipcode == @inquiry_zip')
+df_a_zip_agg = airbnb_zip_info.query('zipcode == @inquiry_zip')
+df_selling_zip = df_selling.query('zip == @inquiry_zip')
+df_rental_zip = df_rental.query('zip == @inquiry_zip')
+df_a_zip_agg
+
+#--------------------main page------------------------------
+st.title(":bar_chart: San Diego Real Estate Dashboard")
+
+#-------------------subhead --------------------------------
+st.markdown("### Rental Info")
+rent_avg = df_rental_zip[df_rental_zip.status == 'Rented'].rented_price.astype(float).mean()
+mask = (df_rental_zip.bedrooms_total == str(inquriry_room_number)) & (df_rental_zip.baths_full == str(inquiry_bath_number))
+
+column1, column2= st.columns(2)
+
+with column1:
+    st.markdown(f"##### {inquiry_zip} Zillow Rental Index: ${df_z_zip['ri__2022_02'].values[0]}")
+
+with column2:
+    st.markdown(f"##### Average Rented Price In Past Three Month: ${rent_avg}")
+
+if (len(df_rental_zip[mask]) == 0):
+    st.markdown(f'No Rental Info for {inquriry_room_number} bedrooms and {inquiry_bath_number} bathrooms Property')
+else:
+    rental_his= px.histogram(df_rental_zip[mask].list_price.astype(float),
+                title = f"      AveragePrice for {inquriry_room_number}b/{inquiry_bath_number}ba Property: ${int(df_rental_zip[mask].list_price.astype(float).mean())}  ")
+    st.plotly_chart(rental_his,template="simple_white",use_container_width = True)
+
+
+st.markdown("""---""")
+# -----------------subhead-------------------------
+st.subheader("Market Value Info")
+
+x=['2022_03_31','2022_05_31','2023_02_28']
+y = df_z_zip.iloc[0][2:5].astype(float)
+df_v_increase = pd.DataFrame({'Date':x, 'Increase Expection':y})
+df_v_increase
+mask = (df_selling_zip.bedrooms_total == str(inquriry_room_number)) & (df_selling_zip.baths_full == str(inquiry_bath_number))
+sold_avg = df_selling_zip[df_selling_zip.status == 'Sold'].sold_price.astype(float).mean()
+
+
+column1, column2 = st.columns(2)
+
+column1.markdown(f"###### {inquiry_zip} Zillow House Value Index: $ {df_z_zip['vi__2022_02_28'].values[0]}")
+column1.markdown(f"##### $ {df_z_zip['vi__2022_02_28'].values[0]}")
+
+column2.markdown(f"###### Average Sold Price In Past Three Month:")
+column2.markdown(f"#####  ${int(sold_avg)}")
+
+
+v_increase_chart = px.bar(df_v_increase,x='Date', y='Increase Expection', title='Zillow Value Increase Forcast :',template="simple_white")
+
+column1, column2 = st.columns(2)
+with column1:
+    st.plotly_chart(v_increase_chart,use_container_width=True)
+
+with column2:
+     df_selling_zip[mask].residential_styles.unique()
+     df_stype = pd.DataFrame([ df_selling_zip[mask].residential_styles,df_selling_zip[mask]['list_price'].astype(float)]).transpose()
+     style_price_fig = px.box(df_stype, x="residential_styles", y="list_price",
+                        title = f'Listing Price Boxplot By Styles of {inquriry_room_number}b/{inquiry_bath_number}ba Property ',
+                        template="simple_white")
+     st.plotly_chart(style_price_fig,use_container_width= True)
+
+
+st.markdown("""---""")
+
+
+# -------------------subhead--------------------------------------
+st.subheader("Airbnb Listing Info")
+df_airbnb_zip = df_airbnb[df_airbnb.zipcode == inquiry_zip]
+
+
+df = df_airbnb.groupby(['zipcode'])['id'].count().sort_values(ascending = False)[:10]
+df = pd.DataFrame({'zipcode':df.index, 'total':df.values})
+
+zip_airbnb_fig = px.bar(df,x='zipcode',y='total',title='Most Popular ZipCode With Aibnb',template="simple_white")
+st.plotly_chart(zip_airbnb_fig)
+
+df_airbnb_zip
+
+#df_airbnb_zip['latitude'].tolist()[0]
+st.pydeck_chart(pdk.Deck(
+     map_style='mapbox://styles/mapbox/light-v9',
+     initial_view_state=pdk.ViewState(
+         latitude=float(df_airbnb_zip['latitude'].tolist()[0]),
+         longitude=float(df_airbnb_zip['longitude'].tolist()[0]),
+         zoom=11,
+         pitch=50,
+     ),
+     layers=[
+         pdk.Layer(
+            'HexagonLayer',
+            data=df_airbnb_zip,
+            get_position='[longitude,latitude]',
+            radius=200,
+            elevation_scale=4,
+            elevation_range=[0, 1000],
+            pickable=True,
+            extruded=True,
+         ),
+         pdk.Layer(
+             'ScatterplotLayer',
+             data=df_airbnb_zip,
+             get_position='[longitude,latitude]',
+             get_color='[200, 30, 0, 160]',
+             get_radius=200,
+         ),
+     ],
+ ))
